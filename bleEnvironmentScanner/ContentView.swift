a865dda6fa4c74e94a540226dce36ace2d054db3
Tom Petz <tom@tjpetz.com>
2020-10-15 14:33:09 -0400

    Initial Commit

---
+++ bleEnvironmentScanner/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  bleEnvironmentScanner
+//
+//  Created by Thomas Petz, Jr. on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
